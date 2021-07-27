e5f1c535ae755cee150161e49a802e7fde01b808
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-07-27 15:11:45 +0300

    adding features

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //logging to host OS console
 console.log(`starting...`)
 
-const { app, BrowserWindow, ipcMain } = require(`electron`)
+const { app, BrowserWindow, ipcMain, Menu, Tray, clipboard, globalShortcut } = require(`electron`)
 const countdown = require(`./countdown`)
 
 let mainWindow
@@ -20,10 +20,90 @@ app.on(`ready`, _ => {
     mainWindow.loadFile(`./src/index.html`)
     //mainWindow.webContents.openDevTools()
 
+
+    const menuTemplate = [{
+            label: app.getName(),
+            submenu: [
+                {
+                    label: `about`,
+                    role: `about` //does not work in ubuntu?
+                },
+                {
+                    type: `separator`
+                },
+                {
+                    label: `quit`,
+                    click: _ => {app.quit()}
+                }
+            ]
+    }]
+
+    const menu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(menu)
+
+    const tray = new Tray(`./src/icon.png`)
+    tray.setContextMenu(
+        Menu.buildFromTemplate(
+            [{label:`<empty>`, enabled: false}]
+        )
+    )
+
+    function addToStack(item, stack) {
+        return [item].concat(stack.length >= 5 ? stack.slice(0,stack.length -1) : stack )
+    }
+
+    function checkClipboardForChange(clipboard, onChange) {
+        let previous = clipboard.readText()
+        let current
+
+        setInterval(_ => {
+            current = clipboard.readText()
+            if (current !== previous) {
+                previous = current
+                onChange(previous)
+            }
+        }, 1000);
+    }
+
+    function formatMaxLength(text) {
+        return text && text.length > 10
+            ? text.substr(0, 7) + `...`
+            : text
+    }
+
+    function createMenuTemplateForStack(stack) {
+        console.log(`mapping:`,stack)
+        const mapped = stack.map(
+            (item,i) => {return {
+                    label:`Copy: ${formatMaxLength(item)}`,
+                    click: _=> clipboard.writeText(item)
+                }}
+        )
+        console.log(`mapped:`,mapped)
+
+        return mapped
+    }
+
+    let stack = []
+    checkClipboardForChange(clipboard, text => {
+        stack = addToStack(text, stack)
+        console.log(`stack`,stack)
+        tray.setContextMenu(
+            Menu.buildFromTemplate(createMenuTemplateForStack(stack))
+        )
+    })
+    
+
     mainWindow.on(`closed`, _ => {
         console.log(`closed...`)
         mainWindow = null
     })
+})
+
+app.on(`will-quit`, _ => {
+    //good practice
+    console.log(`unregistering shortcuts...`)
+    globalShortcut.unregisterAll()
 })
 
 ipcMain.on(`countdown-start`, _ => {
